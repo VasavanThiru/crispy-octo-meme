6f6a553d575701036bcad5d4504440655b3da18e
notoria <notoria@users.noreply.github.com>
2018-12-01 12:30:41 +0100

    Bug corrections, test function added

--- Qnewton.m
+++ Qnewton.m
@@ -1,6 +1,6 @@
 function[H] = Qnewton(x, x0, l, g, g0, j, j0, H0, choix)
     d0 = x - x0;
-    y0 = g + l' * j - (g0 + l' * j0); % error
+    y0 = g + j * l - (g0 + j0 * l);
     if choix == 1
         if y0' * d0 > 0
             H = H0 + y0 * y0' / (y0'*d0) - H0 * d0 * d0' * H0 / (d0' * H0 * d0);
--- SQP.m
+++ SQP.m
@@ -6,29 +6,31 @@ function [x, g, j, k] = SQP(x0, F, eps, kiter, choix)
     m = length(F_0) - 1;
     H = eye(n); % differentiel seconde par rapport a x
     c = 0.1;
-    [g, j] = Gradient(x0, F, repmat(eps, 1, m + 1));
+    [g, j] = Gradient(x0, F, repmat(eps, 1, n));
     [d, l] = pq(H, j', g, -F_0(2:m+1));
     x = x0 + d;
     l0 = l;
+    while k < kiter
     %while k < kiter && norm(x-x0) >= eps % use gradient, why ?
-    while k < kiter && norm(g) >= eps
+    %while k < kiter && norm(g) >= eps
         g0 = g;
         j0 = j;
-        [g, j] = Gradient(x, F, repmat(eps, 1, m + 1));
+        [g, j] = Gradient(x, F, repmat(eps, 1, n));
         H0 = H;
         H = Qnewton(x, x0, l, g, g0, j, j0, H0, choix);
         H = H_modification(H);
         F_0 = F(x0);
         x0 = x;
         [d, l] = pq(H, j', g, -F_0(2:m+1));
-        [x, dx] = Globalisation(x0, l0, d, F, c, rho)
+        [x, dx] = Globalisation(x0, l0, d, F, c, rho);
         d = dx;
         l0 = l;
-        if norm(x - x0) < eps
-            printf("x-difference too small %d %f\n", k, norm(x-x0))
-        end
         k = k + 1;
     end
 end
 
 % F = @(x) [x' * x; sum(x)-1];
+%
+% x0 = [-1; 2; 1; -2; -2];
+%
+ F = @(x) [(x(1)-1)^2+(x(1)-x(2))^2+(x(2)-x(3))^3+(x(3)-x(4))^4+(x(4)-x(5))^4; x(1)+x(2)^2+x(3)^2-3*sqrt(2)-2; x(2)-x(3)^2+x(4)-2*sqrt(2)+2; x(1)*x(5)-2];
